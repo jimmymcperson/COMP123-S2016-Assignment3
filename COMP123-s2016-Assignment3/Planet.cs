366e73ec30bf69b5b5baafcd2ca49d5f167777d3
jimmymcperson <undeadwalrus@hotmail.com>
2016-06-17 22:23:04 -0400

    Initial Commit

---
+++ COMP123-s2016-Assignment3/Planet.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP123_s2016_Assignment3
+{
+    /** <summary>
+     * This abstract class defines a planet.
+     * </summary>
+     * 
+     * @class Planet
+     */
+    public abstract class Planet
+    {
+
+        // PRIVATE INSTANCE VARIABLES =================================================================================================
+
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+        // PUBLIC PROPERTIES =================================================================================================
+
+        public double Diameter
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public double Mass
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public int MoonCount
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public double OrbitalPeriod
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int RingCount
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public double RotationPeriod
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        // CONSTRUCTORS =================================================================================================
+
+        public Planet()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // PRIVATE METHODS =================================================================================================
+
+        // PUBLIC METHODS =================================================================================================
+
+        public string ToString()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ COMP123-s2016-Assignment3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * ==================================================================================================
+ * Author: Jonathan Hao
+ * Student#: 300869273
+ * Date Modified: June 16, 2016
+ * Description: This is a demonstration of class diagrams, abstract classes/methods, and interfaces.
+ * Version: 0.0.1 - Initial commit.
+ * ==================================================================================================
+ */
+namespace COMP123_s2016_Assignment3
+{
+
+     /** <summary>
+     * This is the "driver" class for our program.
+     * </summary>
+     * 
+     * @class Program
+     */
+    public class Program
+    {
+
+        /** <summary>
+         * This is the "main" method.
+         * </summary>
+         * 
+         * @method Main
+         * @param {string[]} args
+         * @returns {void}
+         */
+        public static void Main(string[] args)
+        {
+        }
+
+
+        /** <summary>
+         * This method pauses the program until the user presses any key.
+         * </summary>
+         * 
+         * @method waitForAnyKey
+         * @returns {void}
+         */
+        public void waitForAnyKey()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
